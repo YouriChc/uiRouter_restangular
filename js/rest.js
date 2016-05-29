418a82150f742129c8d6cc40ba74b479fbdeac73
Dahakarful <tsar4le4grand@gmail.com>
2016-05-29 22:13:22 +0200

    first commit

---
+++ js/rest.js
@@ -0,0 +1,8 @@
+angular.module('cycleoflifeApp')
+  .controller('restController', function($scope, Restangular) {
+    // 
+    // Restangular.all('index.com#/birth');
+    //
+    // var ob = Restangular.allUrl('googlers', 'http://www.google.com/').getList();
+    // console.log(ob);
+});
